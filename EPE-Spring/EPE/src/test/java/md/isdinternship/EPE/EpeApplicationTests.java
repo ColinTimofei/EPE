ac423848c80c5b85ec3757eabfc0d2afe40dcf4d
Colin Timofei <ronin2120@gmail.com>
2021-11-09 10:27:32 +0200

    start project

---
+++ EPE-Spring/EPE/src/test/java/md/isdinternship/EPE/EpeApplicationTests.java
@@ -0,0 +1,13 @@
+package md.isdinternship.EPE;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EpeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
